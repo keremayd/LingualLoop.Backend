977d7c2c46a3ef666abf3e9118321302ba631a64
Kerem Aydın <kerem.aydin@turk.net>
2025-06-21 14:15:46 +0300

    bugfix v2 in UploadPhotoByIdCommandHandler

--- Service/Handlers/Commands/UploadPhotoByIdCommandHandler.cs
+++ Service/Handlers/Commands/UploadPhotoByIdCommandHandler.cs
@@ -1,4 +1,6 @@
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Postgres;
 using Postgres.Abstractions;
 using Postgres.Models;
 using Service.DataTransferObjects.Requests;
@@ -9,20 +11,22 @@ namespace Service.Handlers.Commands;
 public class UploadPhotoByIdCommandHandler : IRequestHandler<UploadPhotoByIdRequest, UploadPhotoByIdResponse>
 {
     private readonly ILingualLoopGenericRepository<User> _userRepository;
+    private readonly LingualLoopContext _context;
     
     public UploadPhotoByIdCommandHandler(ILingualLoopGenericRepository<User> userRepository)
     {
         _userRepository = userRepository;
+        _context = userRepository.GetDbContext();
     }
     
     public async Task<UploadPhotoByIdResponse> Handle(UploadPhotoByIdRequest request, CancellationToken cancellationToken)
     {
-        var user =  await _userRepository.FirstAsync(x => x.Id.Equals(request.Id), user => new User() { Id = user.Id, ProfilePhoto = user.ProfilePhoto });
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.Id.Equals(request.Id), cancellationToken: cancellationToken);
         
         user!.ProfilePhoto = request.PhotoUrl;
         
-        _userRepository.Update(user);
-        await _userRepository.SaveChangesAsync(cancellationToken);
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync(cancellationToken);
 
         return new UploadPhotoByIdResponse()
         {
